1bc158657eb25f02ea9e1daf4b8dd715f5c841bc
Muhammed Ziyad <muhdziyadu@gmail.com>
2024-01-19 11:45:16 +0530

    modal buttons changed to each component

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -31,24 +31,7 @@ function Modal({showModal,setShowModal,action}) {
               </div>
 
               <div className="relative p-6 flex-auto">
-                {action=='addItem'?<AddItem/>:action=='insertData'?<InsertData/>:null}
-              </div>
-
-              <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                <button
-                  className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                  type="button"
-                  onClick={() => setShowModal(false)}
-                >
-                  Close
-                </button>
-                <button
-                  className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                  type="button"
-                  onClick={() => setShowModal(false)}
-                >
-                  Save Changes
-                </button>
+                {action=='addItem'?<AddItem setShowModal={setShowModal}/>:action=='insertData'?<InsertData setShowModal={setShowModal}/>:null}
               </div>
             </div>
           </div>
